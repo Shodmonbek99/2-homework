01dbdc0d3fb46e74e8afdd8519c9f9f1d58b3594
Shodmonbek-06 <shodmonbekakbarov02@gmail.com>
2024-05-13 20:53:53 +0500

    2-homework

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import ProtectedRoutes from "./components";
+
+import MyLayout from "./layout/MyLayout";
+import { Navbar, Footer } from "./components";
+import { Home, About, Contact, Signup, Login, Create } from "./pages";
+
+function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MyLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/create",
+          element: <Create />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
+  return <RouterProvider />;
+}
+
+
+export default App;
